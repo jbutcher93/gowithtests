3b4ed74784c2c5a57c210cd74e476362dfc3aaa3
jbutcher93 <1butcherjohn@gmail.com>
2024-05-08 12:45:56 -0400

    refactor hello-world

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -1,16 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-const englishHelloPrefix = "Hello, "
+const (
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch strings.ToLower(language) {
+	case "spanish":
+		prefix = spanishHelloPrefix
+	case "french":
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", "Spanish"))
 }

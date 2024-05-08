fae286b341a9048d136892914478a49871aae6e1
jbutcher93 <1butcherjohn@gmail.com>
2024-05-08 12:46:05 -0400

    integers package

---
+++ integers/adder_test.go
@@ -0,0 +1,14 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+	if sum != expected {
+		fmt.Printf("expected %q, but got %q", expected, sum)
+	}
+}
